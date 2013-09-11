5000946f45c861b36067eb5bca288c823e933bbd
EliePse <eliepse.meignan@gmail.com>
2013-09-11 12:35:55 +0200

    Ajout du fichier de configuration
    Ce fichier permettra l'implémentation des principales variables globales
    nécessaires.

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+/* Il s'agit, ici, des infos de connection à la base de donnée */
+
+if(false){
+	
+	define("DB_HOST", "mysql51-72.perso");
+	define("DB_PORT", "3306");
+	define("DB_NAME", "eliepsesql");
+	define("DB_USERNAME", 'eliepsesql');
+	define("DB_PASSWORD", 'pcem84');
+	
+}else{
+	define("DB_HOST", "localhost");
+	define("DB_PORT", "");
+	define("DB_NAME", "anniv");
+	define("DB_USERNAME", "root");
+	define("DB_PASSWORD", "");
+	
+}
