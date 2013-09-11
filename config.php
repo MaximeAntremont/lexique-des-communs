5e3e74f8be0984464ef9ea3ec4858cb96ca4fbe8
EliePse <eliepse.meignan@gmail.com>
2013-09-12 00:36:09 +0200

    Suppression de données personnelles
    Merci de faire attention, ne faites pas la même erreur que moi.

--- config.php
+++ config.php
@@ -2,23 +2,12 @@
 
 /* Il s'agit, ici, des infos de connection à la base de donnée */
 
-/* true = en ligne | false = en local
-	merci de ne pas faire de commit si la modification implique seulement
-	le switch de vette valeur.
+/* 
+	NE SURTOUT PAS METTRE D'INFORMATION PERSONNELLE DANS CE FICHIER !!!! (ex: mot de passe)
 */
-if(false){
-	
-	define("DB_HOST", '');
-	define("DB_PORT", '');
-	define("DB_NAME", '');
-	define("DB_USERNAME", '');
-	define("DB_PASSWORD", '');
-	
-}else{
+
 	define("DB_HOST", 'localhost');
 	define("DB_PORT", '');
 	define("DB_NAME", '');
 	define("DB_USERNAME", 'root');
 	define("DB_PASSWORD", '');
-	
-}
