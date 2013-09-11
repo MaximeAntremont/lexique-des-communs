11e9197dbb4aa99e6c6afbcfe467b51cdd6c4954
EliePse <eliepse.meignan@gmail.com>
2013-09-12 00:23:12 +0200

    Mise en place de la classe Entry
    Classe non testé pour cause de BDD encore indisponible.

---
+++ class/entry.class.php
@@ -0,0 +1,55 @@
+<?php
+
+class Entry 
+{
+	private $id,
+			$val,
+			$create_date;
+	
+	/***************
+	 * CONSTRUCTOR
+	 ***************/
+	 
+	public function __construct (array $donnees)
+	{
+		$this->hydrate($donnees);
+	}
+	
+	public function hydrate(array $donnees)
+	{
+		foreach ($donnees as $key => $value)
+		{
+			$method = preg_replace("#^entry_#", "", $key);
+			if (method_exists($this, $method))
+			{
+			  $this->$method($value);
+			}
+		}
+	}
+	
+	/********************
+	 * getters & setters
+	 ********************/
+	
+	public function id ($val = null){
+		if(isset($val) && is_numeric($val) && $val >= 0) $this->id = $val;
+		return $this->id;
+	};
+	public function val ($val = null){
+		if(isset($val) && is_string($val) && strlen($val) <= 50) $this->val = $val;
+		return $this->val;
+	};
+	public function create_date ($val = null){
+		if(isset($val) && is_numeric($val) && $val >= 0) $this->create_date = $val;
+		return $this->create_date;
+	};
+	
+	/*
+	
+		TODO permettre l'ajout de ressources
+		TODO système de navigation dans les ressources (recherche, fetch, reset/place cursor, ...)
+		TODO développer le manager global gérant tout d'un coup (moins chiant qu'un manger par classe)
+	
+	*/
+	
+}
