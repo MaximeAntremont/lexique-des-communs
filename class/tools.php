12d9fd4d2767328bc542bfe18fb80f3005a31425
EliePse <eliepse.meignan@gmail.com>
2013-09-13 08:02:21 +0200

    .gitignore (add) & tools (modif)
    - adding "test.php" in .gitignore
    - tools.php: adding port value fort connection

--- class/tools.php
+++ class/tools.php
@@ -1,12 +1,12 @@
 <?php
 
-function getConnection (){
-	try {
-		$db = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";",DB_USERNAME,DB_PASSWORD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		return $db;
-	} catch (PDOException $e) {
-		echo 'Échec lors de la connexion : ' . $e->getMessage();
-		return false;
+	function getConnection (){
+		try {
+			$db = new PDO("mysql:host=".DB_HOST.";port=".DB_PORT.";dbname=".DB_NAME.";",DB_USERNAME,DB_PASSWORD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			return $db;
+		} catch (PDOException $e) {
+			echo 'Échec lors de la connexion : ' . $e->getMessage();
+			return false;
+		}
 	}
-}
