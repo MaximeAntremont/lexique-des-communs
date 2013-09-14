792b4ca3d1d9fd79bd67e7e97b4f80ea782c23b8
EliePse <elie.meignan@yahoo.fr>
2013-09-14 16:17:09 +0200

    oubli

---
+++ config.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+/* 
+	NE SURTOUT PAS METTRE D'INFORMATION PERSONNELLE DANS CE FICHIER !!!! (ex: mot de passe)
+	A DECOMMENTER & COMPLETER SEULEMENT APRES MISE EN SERVICE SUR LE SERVEUR EN LIGNE
+*/
+	// define("DB_HOST", '');
+	// define("DB_PORT", '');
+	// define("DB_NAME", '');
+	// define("DB_USERNAME", '');
+	// define("DB_PASSWORD", '');
+
+	header('Content-type: text/html; charset=utf-8');
