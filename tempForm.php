531633fd96b5d4962240677e4881591005ed33ae
EliePse <elie.meignan@yahoo.fr>
2013-09-14 16:30:31 +0200

    Réception du formulaire (tempForm.php)

---
+++ tempForm.php
@@ -0,0 +1,31 @@
+<?php
+
+include_once('../config.php');
+include_once('class/tools.php');
+include_once('class/entry.class.php');
+include_once('class/ressource.class.php');
+include_once('class/log.class.php');
+include_once('class/link.class.php');
+include_once('class/user.class.php');
+include_once('class/manager.class.php');
+
+//partie pour la sécurité
+foreach($_POST as $key => $val){
+	$_POST[$key] = htmlspecialchars(trim($val));
+}
+
+//partie pour la création des objets
+$entry = new Entry($_POST);
+$ressource = new Ressource($_POST);
+$link = new Link($_POST);
+
+if($db = getConnection()){
+	$manager = new Manager($db);
+	
+	if(!$manager->isEntrySet($entry)) $manager->sendNewEntry($entry);
+	$manager->sendNewRessource($ressource);
+	if(!$manager->isLinkSet($link)) $manager->sendNewLink($link);
+	
+}
+
+header("location: index.php");
