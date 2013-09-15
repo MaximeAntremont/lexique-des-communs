6c9ea081d965a7396aaec974462e2c8d7c0738e1
MaximeAntremont <maxime.antremont@gmail.com>
2013-09-15 13:42:44 +0200

    Header de la visualisation

---
+++ visualisation.header.php
@@ -0,0 +1,19 @@
+<?php
+	include_once('../config.php');
+	include_once('config.php');
+	include_once('class/tools.php');
+	include_once('class/entry.class.php');
+	include_once('class/ressource.class.php');
+	include_once('class/log.class.php');
+	include_once('class/link.class.php');
+	include_once('class/user.class.php');
+	include_once('class/manager.class.php');
+	
+	$manager = new Manager(getConnection());
+?>
+<!doctype html>
+<html>
+	<head>
+		<link rel="stylesheet" href="css/visualisation.css" type="text/css">
+	</head>
+	<body>
