f1d2d61683b6b216e66bf56bd04e108c08e2386c
MaximeAntremont <maxime.antremont@gmail.com>
2013-09-15 14:26:53 +0200

    drawIndex() : sous-index de la lettre

--- class/tools.php
+++ class/tools.php
@@ -11,25 +11,34 @@
 		}
 	}
 	
-	function drawIndex($letter = 'A', $isWritten = array()){
+	function drawIndex($selected = null, $letter = 'A', $isWritten = array()){
 		$isWritten[$letter] = false;
+		global $manager;
 		global $entries;
 		
 		if($letter != 'AA'){
 			foreach($entries as $entry){
 				if(strpos($entry->val(), $letter) === 0 && isset($isWritten[$letter]) && $isWritten[$letter] == false){
-					echo('<a href="">'.$letter.'</a>'.(($letter == 'Z')?'':'-'));
+					echo('<a '.((strpos($selected,$letter) === 0)?'class="selected"':'').' href="visualisation.php?letter='.$letter.'">'.$letter.'</a>'.(($letter == 'Z')?'':'-'));
 					$isWritten[$letter] = true;
 					$letter++;
 				}
 			}
-			
 			if(isset($isWritten[$letter]) && $isWritten[$letter] == false){
 				echo('<span>'.$letter.'</span>'.(($letter == 'Z')?'':'-'));
 				$isWritten[$letter] = true;
 				$letter++;
 			}
 				
-			drawIndex($letter);
+			drawIndex($selected, $letter);
+		}
+		else if($selected != null && strpos($selected,$letter) != -1){
+			$selectableEntries = $manager -> getEntryByLetter($selected);
+			echo('<br /><h1>'.$selectableEntries[0]->val().'</h1>');
+			echo('<ul>');
+			foreach($selectableEntries as $selectableEntry){
+				echo('<li>'.$selectableEntry -> val().'</li>');
+			}
+			echo('</ul>');
 		}
 	}
