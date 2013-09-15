708dc4bd584520563ce1d586582da37c0ef1fe59
EliePse <eliepse.meignan@gmail.com>
2013-09-15 18:59:05 +0200

    Ajout de getEntry

---
+++ utils/getEntry.util.php
@@ -0,0 +1,32 @@
+﻿<?php
+	
+	include_once('../../config.php');
+	include_once('../config.php');
+	include_once('../class/tools.php');
+	include_once('../class/entry.class.php');
+	include_once('../class/ressource.class.php');
+	include_once('../class/log.class.php');
+	include_once('../class/link.class.php');
+	include_once('../class/user.class.php');
+	include_once('../class/category.class.php');
+	include_once('../class/manager.class.php');
+	
+	$manager = new Manager(getConnection());
+	
+	if(!$manager->isHS() && !empty($_POST['entry_id']) && is_string($_POST['entry_id'])){
+		
+		
+		$entry_id = htmlspecialchars($_POST['entry_id']);
+		
+		if($entry = $manager->getEntryBy_id( $entry_id )){
+			
+			echo json_encode($entry->getArray());
+			
+		}else{
+			
+			return json_encode( array('error' => false) );
+			
+		}
+		
+		
+	}
