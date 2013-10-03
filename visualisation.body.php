9b40c5fd7f52b302b4d3a60ce20832032cf59f1f
EliePse <eliepse.meignan@gmail.com>
2013-10-03 23:07:41 +0200

    [Version a2r01]

--- visualisation.body.php
+++ visualisation.body.php
@@ -18,7 +18,7 @@
 	</div>
 	<div id="bottom_panel">
 		<div id="alertBug" class="panel_button" style="margin-left: 40px;border-left: 1px solid rgb(200,200,200);"></div>
-		<div id="infos" class="panel_button">v. a2r00</div>
+		<div id="infos" class="panel_button">v. a2r01</div>
 		<div>Lexique: <?php echo $_SESSION['lexique_name']; ?></div>
 	</div>
 	
